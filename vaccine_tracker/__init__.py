49001cdd7d9a3a2b8abe9ab9b01cd15021653434
Diego Alloza González <sralloza@gmail.com>
2021-08-24 21:07:12 +0200

    chore: add dotenv

--- vaccine_tracker/__init__.py
+++ vaccine_tracker/__init__.py
@@ -1,1 +1,9 @@
-__version__ = '0.1.0'
+from dotenv import load_dotenv
+from pathlib import Path
+
+__version__ = "0.1.0"
+
+DOTENV_PATH = Path(__file__).with_name(".env")
+
+if DOTENV_PATH.is_file():
+    load_dotenv(DOTENV_PATH)
